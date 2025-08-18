95131d4aa497df7d74def6cea0ff25bd4bcc2294
The-shadow-gamers <glitched.cat9@gmail.com>
2025-08-17 21:36:33 -0400

    Delete netlify/functions/health.js

--- netlify/functions/health.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json", "Cache-Control": "no-store" },
-    body: JSON.stringify({ ok: true })
-  };
-};
